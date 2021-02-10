089e8bed0fa13534fb806ff9d584d9b1fc5db221
Hannes Olszewski <kontakt@hannes-olszewski.dev>
2021-02-10 12:02:16 +0100

    Add C++ template with CMake and googletest

---
+++ templates/c-plus-plus/src/Calculator.h
@@ -0,0 +1,17 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+    public:
+    /**
+     * Return the sum of a and b
+     * Throws an exception if a and/or b have illegal resultTypes or the result cannot be calculated
+     *
+     * @param a first addend
+     * @param b second addend
+     * @return sum of a and b
+     */
+    int add(int a, int b);
+};
+
+#endif // CALCULATOR_H
---
+++ templates/c-plus-plus/test/CalculatorTest.cpp
@@ -0,0 +1,37 @@
+#include<limits.h>
+#include"gtest/gtest.h"
+#include"Calculator.h"
+using namespace std;
+
+class CalculatorTest : public ::testing::Test {
+    protected:
+    
+        CalculatorTest() {
+            calculator = new Calculator();
+        }
+
+        ~CalculatorTest() override {
+            delete calculator;
+        }
+
+        Calculator* calculator;
+};
+
+TEST_F(CalculatorTest, AdditionWithRegularNumbers) {
+    int a = -3;
+    int b = 7;
+    EXPECT_EQ(calculator->add(a, b), a + b);
+}
+
+TEST_F(CalculatorTest, ValidAdditionsWithExtremeValues) {
+    EXPECT_EQ(calculator->add(INT_MAX, 0), INT_MAX);
+    EXPECT_EQ(calculator->add(INT_MIN, 0), INT_MIN);
+    EXPECT_EQ(calculator->add(INT_MAX, -1 * INT_MIN), 0);
+}
+
+TEST_F(CalculatorTest, InvalidAdditionsThatWouldResultInAnIntegerOverflow) {
+    EXPECT_ANY_THROW(calculator->add(INT_MAX, 1));
+    EXPECT_ANY_THROW(calculator->add(INT_MAX - 10, 11));
+    EXPECT_ANY_THROW(calculator->add(INT_MIN, -1));
+    EXPECT_ANY_THROW(calculator->add(INT_MIN + 10, -11));
+}
